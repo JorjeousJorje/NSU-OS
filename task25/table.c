fcf3cb994b130b24b7af8c0e01bd1ddd5608e68c
JorjeousJorje <71111031+JorjeousJorje@users.noreply.github.com>
2020-12-10 14:10:34 +0700

    updated

--- task25/table.c
+++ task25/table.c
@@ -8,29 +8,31 @@
 
 #define BUF_SIZE 100
 
-int write_process(int pipe_fd[2]) {
+int write_process(int pipe_fd[2])
+{
     if (close(pipe_fd[0]) == -1) {
-        fprintf(stderr, "close() error in %d:\n", getpid());
+        fprintf(stderr, "write close() error in %d:\n", getpid());
         return EXIT_FAILURE;
     }
     const char* message = "Hello, another process!";
     if (write(pipe_fd[1], message, strlen(message)) == -1) {
         fprintf(stderr, "write() error in %d:\n", getpid());
         if (close(pipe_fd[1]) == -1) {
-            fprintf(stderr, "close() error in %d:\n", getpid());
+            fprintf(stderr, "write close() error in %d:\n", getpid());
         }
         return EXIT_FAILURE;
     }
     if (close(pipe_fd[1]) == -1) {
-        fprintf(stderr, "close() error in %d:\n", getpid());
+        fprintf(stderr, "write close() error in %d:\n", getpid());
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
 
-int read_process(int pipe_fd[2]) {
+int read_process(int pipe_fd[2])
+{
     if (close(pipe_fd[1]) == -1) {
-        fprintf(stderr, "close() error in %d:\n", getpid());
+        fprintf(stderr, "read close() error in %d:\n", getpid());
         return EXIT_FAILURE;
     }
     char buf[BUF_SIZE];
@@ -38,7 +40,7 @@ int read_process(int pipe_fd[2]) {
     if (read_count == -1 && errno != EINTR) {
         fprintf(stderr, "read() error in %d:\n", getpid());
         if (close(pipe_fd[0]) == -1) {
-            fprintf(stderr, "close() error in %d:\n", getpid());
+            fprintf(stderr, "read close() error in %d:\n", getpid());
         }
         return EXIT_FAILURE;
     }
@@ -53,25 +55,27 @@ int read_process(int pipe_fd[2]) {
     printf("Upper formatted text: %s\n", read_string);
 
     if (close(pipe_fd[0]) == -1) {
-        fprintf(stderr, "close() error in %d:\n", getpid());
+        fprintf(stderr, "read close() error in %d:\n", getpid());
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
 
-int close_pipe(int pipe_fd[2]) {
+int close_pipe(int pipe_fd[2])
+{
     if (close(pipe_fd[0]) == -1) {
-        fprintf(stderr, "close() error in %d:\n", getpid());
+        fprintf(stderr, "main close() error in %d:\n", getpid());
         return EXIT_FAILURE;
     }
     if (close(pipe_fd[1]) == -1) {
-        fprintf(stderr, "close() error in %d:\n", getpid());
+        fprintf(stderr, "main close() error in %d:\n", getpid());
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
 
-int wait_for_pend(pid_t pid, int status){
+int wait_for_pend(pid_t pid, int status)
+{
     do {
         pid_t terminated_child_pid = waitpid(pid, &status, 0);
         if (terminated_child_pid == -1) {
@@ -92,47 +96,47 @@ int wait_for_pend(pid_t pid, int status){
     return EXIT_SUCCESS;
 }
 
+
 int main(int argc, char *argv[]) {
-    int status1, status2;
     int pipe_fd[2];
-    char buf[BUF_SIZE];
+    int status = 0;
 
     if (pipe(pipe_fd) == -1) {
-        fprintf(stderr, "Error at pipe\n");
-        return -1;
+        fprintf(stderr, "pipe() error in %d:\n", getpid());
+        return EXIT_FAILURE;
     }
 
     pid_t pid1 = fork();
-    if (pid1 == -1) {
-        fprintf(stderr, "Error at fork() 1\n");
+    if(pid1 == -1) {
+        fprintf(stderr, "fork() error in %d:\n", getpid());
         close_pipe(pipe_fd);
         return EXIT_FAILURE;
     }
-    if (pid1 == 0) {
+    if(pid1 == 0) {
         return write_process(pipe_fd);
     } else {
-        pid_t pid2 = fork();
-        if (pid2 == -1) {
-            fprintf(stderr, "fork() error in %d:\n", getpid());
+        if(wait_for_pend(pid1, status) == EXIT_FAILURE) {
             close_pipe(pipe_fd);
-            wait_for_pend(pid1, status1);
             return EXIT_FAILURE;
         }
-        if (pid2 == 0) {
-            return read_process(pipe_fd);
-        } else {
-            if(wait_for_pend(pid1, status1) == EXIT_FAILURE) {
-                close_pipe(pipe_fd);
-                return EXIT_FAILURE;
-            }
-            if(wait_for_pend(pid2, status2) == EXIT_FAILURE) {
-                close_pipe(pipe_fd);
-                return EXIT_FAILURE;
-            }
+        status = 0;
+    }
+
+    pid_t pid2 = fork();
+    if(pid2 == -1) {
+        fprintf(stderr, "fork() error in %d:\n", getpid());
+        close_pipe(pipe_fd);
+        return EXIT_FAILURE;
+    }
+    if(pid2 == 0) {
+        return read_process(pipe_fd);
+    } else {
+        if(wait_for_pend(pid2, status) == EXIT_FAILURE) {
+            close_pipe(pipe_fd);
+            return EXIT_FAILURE;
         }
     }
 
     close_pipe(pipe_fd);
     return EXIT_SUCCESS;
 }
-
